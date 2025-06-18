413a8e7034706297997880f64f78532a35c8f271
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-06-18 08:03:43 +0530

    Data Validation and schema file.

--- pollution_forecasting/entity/config_entity.py
+++ pollution_forecasting/entity/config_entity.py
@@ -33,3 +33,31 @@ class DataIngestionConfig:
         self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
         self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
         self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME
+
+
+class DataValidationConfig:
+    """
+    Configuration class for the data validation process that defines file paths for storing 
+    validation results, drift reports, and valid/invalid data sets.
+    """
+    
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        """
+        Initialize the DataValidationConfig with necessary file paths.
+        
+        Args:
+            training_pipeline_config (TrainingPipelineConfig): Configuration object containing base artifact directory and other pipeline settings.
+        """
+
+        self.data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME)
+        self.valid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_VALID_DIR)
+        self.invalid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_INVALID_DIR)
+        self.valid_train_file_path: str = os.path.join(self.valid_data_dir, training_pipeline.TRAIN_FILE_NAME)
+        self.valid_test_file_path: str = os.path.join(self.valid_data_dir, training_pipeline.TEST_FILE_NAME)
+        self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TRAIN_FILE_NAME)
+        self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TEST_FILE_NAME)
+        self.drift_report_file_path: str = os.path.join(
+            self.data_validation_dir,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
+        )
