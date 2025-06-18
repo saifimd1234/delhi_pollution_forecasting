324fb630dc8bb9c1ba431acb65c79bcaec0aeaf6
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-06-19 00:38:24 +0530

    Update main and run.
    These are the result of Data Ingestion, Data Validation, Data Transformation.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from pollution_forecasting.components.data_ingestion import DataIngestion
 from pollution_forecasting.components.data_validation import DataValidation
 from pollution_forecasting.exception.exception import PollutionException
+from pollution_forecasting.components.data_transformation import DataTransformation
 from pollution_forecasting.logging.logger import logging
 from pollution_forecasting.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from pollution_forecasting.entity.artifact_entity import DataIngestionArtifact
-from pollution_forecasting.entity.config_entity import TrainingPipelineConfig
+from pollution_forecasting.entity.config_entity import TrainingPipelineConfig, DataTransformationConfig
 
 import sys
 
@@ -28,5 +29,13 @@ if __name__ == '__main__':
         print(dataingestionartifact)
         print(data_validation_artifact)
 
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        logging.info("Initiate the data transformation.")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation process completed successfully.")
+        print(data_transformation_artifact)
+   
+    
     except Exception as e:
         raise PollutionException(e, sys)
