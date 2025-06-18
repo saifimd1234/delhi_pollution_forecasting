7302dc1ca2853cde6bc6859631c54380ee7d5d63
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-06-18 09:13:04 +0530

    Update main.py to include DataValidation components.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from pollution_forecasting.components.data_ingestion import DataIngestion
+from pollution_forecasting.components.data_validation import DataValidation
 from pollution_forecasting.exception.exception import PollutionException
 from pollution_forecasting.logging.logger import logging
-from pollution_forecasting.entity.config_entity import DataIngestionConfig
+from pollution_forecasting.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from pollution_forecasting.entity.artifact_entity import DataIngestionArtifact
 from pollution_forecasting.entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -16,6 +18,15 @@ if __name__ == '__main__':
         logging.info("Data Ingestion process completed successfully.")
 
         print(dataingestionartifact)
+        print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("Initiate the data validation.")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        
+        logging.info("Data Validation process completed successfully.")
+        print(dataingestionartifact)
+        print(data_validation_artifact)
 
     except Exception as e:
         raise PollutionException(e, sys)
